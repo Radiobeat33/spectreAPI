cae1c2805c0d90719d1e956c354f1b4b3ad8dace
Steven Williams <Steven30135@gmail.com>
2015-02-24 19:57:36 -0800

    Added even more cors

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongo: {
-    url: process.env.MONGOLAB_URI
+    url: process.env.MONGOLAB_URI 
   },
   //productionURL: process.env.PRODUCTION_URL,
   JWT_SECRET: process.env.JWT_SECRET,
