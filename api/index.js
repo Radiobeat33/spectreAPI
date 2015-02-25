761b933807757bec3c9e97ee421e32b5286962bd
Steven Williams <Steven30135@gmail.com>
2015-02-24 19:05:38 -0800

    basic server layout with data test... really

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ router.use('/grants', require('./grants'));
 // router.use('/integrations', require('./integrations'));
 // router.use('/settings', require('./settings'));
 // router.use('/widgets', require('./widgets'));
-
 module.exports = router;
 // module.exports = function(app){
 //   //create app.use per new routes
